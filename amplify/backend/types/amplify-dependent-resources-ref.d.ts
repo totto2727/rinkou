9b372e5b4266e1fe2b77c9ffccf3b99e50fbfe12
totto2727 <kaihatu.totto2727@gmail.com>
2022-07-20 15:47:48 +0900

    プロジェクト開始

---
+++ amplify/backend/types/amplify-dependent-resources-ref.d.ts
@@ -0,0 +1,33 @@
+export type AmplifyDependentResourcesAttributes = {
+  auth: {
+    rinkoub0680ea1: {
+      IdentityPoolId: 'string'
+      IdentityPoolName: 'string'
+      UserPoolId: 'string'
+      UserPoolArn: 'string'
+      UserPoolName: 'string'
+      AppClientIDWeb: 'string'
+      AppClientID: 'string'
+    }
+  }
+  function: {
+    rinkou694283edDefineAuthChallenge: {
+      Name: 'string'
+      Arn: 'string'
+      LambdaExecutionRole: 'string'
+      Region: 'string'
+    }
+    rinkou694283edCreateAuthChallenge: {
+      Name: 'string'
+      Arn: 'string'
+      LambdaExecutionRole: 'string'
+      Region: 'string'
+    }
+    rinkoub0680ea1PostConfirmation: {
+      Name: 'string'
+      Arn: 'string'
+      LambdaExecutionRole: 'string'
+      Region: 'string'
+    }
+  }
+}
